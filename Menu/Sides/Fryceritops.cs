5b1a7b326e43f5532da6846a89ffd157025f388f
Sam Waris <swaris@ksu.edu>
2019-09-17 13:14:30 -0700

    Updates

---
+++ Menu/Sides/Fryceritops.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Sides
+{
+    public class Fryceritops : Side
+    {
+        private Size size;
+        public override Size Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+                switch(size)
+                {
+                    default:
+                    case Size.Small:
+                        Price = 0.99;
+                        Calories = 222;
+                        break;
+                    case Size.Medium:
+                        Price = 1.45;
+                        Calories = 365;
+                        break;
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 480;
+                        break;
+                }
+            }
+        }
+
+        public Fryceritops()
+        {
+            base.Calories = 222;
+            base.Price = 0.99;
+        }
+    }
+}
